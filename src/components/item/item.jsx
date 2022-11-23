8a391ee180ee5d81e678988691ace120dde63a80
August McAllister <august.chm@gmail.com>
2022-11-23 15:27:11 -0600

    Cleanup, formatting & styling

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import '../ShoppingList/ShoppingList.css';
+import './item.css'
 
 
 //put in placeholders here for further down the line
@@ -40,16 +40,16 @@ function item({item, fetchList}) {
 
     return (
         <>
-                    {item.map(item => (
-                    <section key={item.id} className='list-table'>
-                        <p>{item.name}</p>
-                        <p>{item.quantity}{item.unit}</p> 
-                        {item.is_purchased === false && <button id={item.id} type="add" onClick={() => buyItem(item.id)}>Buy</button> }
-                        {item.is_purchased === false && <button type="delete" onClick={() => deleteItem(item.id)} >Remove</button> } 
-                        {item.is_purchased === true && <label>Purcased</label>}
-                        {/* <button type="add">Buy</button>
-                        <button type="delete">Remove</button> */}
-                    </section>))}
+          {item.map(item => (
+          <section key={item.id} className='list-table'>
+              <p>{item.name}</p>
+              <p>{item.quantity} {item.unit}</p> 
+              {item.is_purchased === false && <button id={item.id} type="add" onClick={() => buyItem(item.id)}>Buy</button> }
+              {item.is_purchased === false && <button type="delete" onClick={() => deleteItem(item.id)} >Remove</button> } 
+              {item.is_purchased === true && <label>Purcased</label>}
+              {/* <button type="add">Buy</button>
+              <button type="delete">Remove</button> */}
+          </section>))}
         </>
     )
 }
