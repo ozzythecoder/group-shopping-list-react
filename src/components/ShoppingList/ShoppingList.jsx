51e13157b2d595896441c1011ea23787c676a9ca
Victor Llapa <victorllapa@gmail.com>
2022-11-23 13:59:27 -0600

    Added the functionality to the reset button as well as testing to the button

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import './ShoppingList.css';
 import Item from '../item/item';
+import axios from 'axios';
 //put in placeholders here for further down the line
 //should be able to render here with the placeholders
-function ShoppingList({item}) {
+function ShoppingList({ item, fetchList }) {
+	const handleReset = () => {
 
-    return (
-        <div>
-            <h2>Shopping List</h2>
-            <button type="reset">Reset</button>
-            <button type="delete">Clear</button>
-            <Item item={item} />
-        </div>
-    )
+		axios.put('/shopping/reset')
+			.then((response) => {
+				console.log('Resetting All Purchases to False. In axios.put (/shopping/reset)');
+				fetchList();
+			}).catch((error) => {
+				console.log('Not able to Reset all Purchases.', error);
+			})
+	}
+
+
+
+
+	return (
+		<div>
+			<h2>Shopping List</h2>
+			<button type="reset" onClick={handleReset}>Reset</button>
+			<button type="delete">Clear</button>
+			<Item item={item} />
+		</div>
+	)
 }
 
 export default ShoppingList;
